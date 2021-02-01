2b4be821e206971f395c7dd1679272e7e41e9a69
KellyEgesa <kelly.egesa@gmail.com>
2021-02-01 20:55:36 +0300

    Added getters and setters  tests

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -4,9 +4,31 @@ public class User {
     private String username;
     private int userId;
     private String fireBaseUserId;
+    private String email;
 
-    public User(String username, String fireBaseUserId){
+    public User(String username, String fireBaseUserId, String email){
         this.fireBaseUserId = fireBaseUserId;
         this.username = username;
+        this.email = email;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFireBaseUserId() {
+        return fireBaseUserId;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -7,12 +7,38 @@ import static org.junit.Assert.*;
 public class UserTest {
 
     private User setUpUser() {
-        return new User("KellyEgesa", "fBAda12432wre");
+        return new User("KellyEgesa", "fBAda12432wre", "kelly.egesa@gmail.com");
     }
 
     @Test
     public void userInstantiatesCorrectly() {
         User testUser = setUpUser();
         assertTrue(testUser instanceof User);
+    }
+
+    @Test
+    public void setUserIdSetsIdCorrectly_1() {
+        User testUser = setUpUser();
+        testUser.setUserId(1);
+        assertEquals(1, testUser.getUserId());
+    }
+
+    @Test
+    public void getUsernameGetsUsernameCorrectly_KellyEgesa() {
+        User testUser = setUpUser();
+        assertEquals("KellyEgesa", testUser.getUsername());
+    }
+
+    @Test
+    public void getFireBaseUserIdGetsFireBaseUserIdCorrectly_fBAda12432wre() {
+        User testUser = setUpUser();
+        assertEquals("fBAda12432wre", testUser.getFireBaseUserId());
+
+    }
+
+    @Test
+    public void getEmailGetsEmailCorrectly() {
+        User testUser = setUpUser();
+        assertEquals("kelly.egesa@gmail.com", testUser.getEmail());
     }
 }
